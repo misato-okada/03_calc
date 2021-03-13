3fead19bae5da1b034aab962c45ceb9722ad8cd0
misato-okada <bamoja3310@gmail.com>
2021-03-13 22:19:36 +0900

    内容追加

---
+++ calc_form.php
@@ -0,0 +1,30 @@
+<!-- <?php
+$sum  = $num1 + $num2;
+$diff = $num1 - $num2;
+$pro  = $num1 * $num2; 
+$quo  = $num1 / $num2;
+?> -->
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>計算アプリ</h1>
+    <h2>計算式を入力して下さい</h2>
+    <form action="answer.php" method="post">
+        <input type="number" name="num1" required>
+        <select name="operator" id="">
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="×">×</option>
+            <option value="÷">÷</option>
+        </select>
+        <input type="number" name="num2" required>
+        <input type="submit" value="計算">
+    </form>
+</body>
+</html>
