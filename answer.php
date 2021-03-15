fe4b30a7837e15dedd1bdfedb5d2e48ac7c8654d
misato-okada <bamoja3310@gmail.com>
2021-03-15 21:43:06 +0900

    内容修正

--- answer.php
+++ answer.php
@@ -1,33 +1,41 @@
+<?php
+    
+    $num1 = $_POST['num1'];
+    $num2 = $_POST['num2'];
+    $operator = $_POST['operator'];
+
+    $sum  = $num1 + $num2;
+    $diff = $num1 - $num2;
+    $pro  = $num1 * $num2; 
+    $quo  = $num1 / $num2;
+
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>計算アプリ</title>
 </head>
 <body>
     <h1>計算結果</h1>
     <?php
-
-    $sum  = $_POST['num1'] + $_POST['num2'];
-    $diff = $_POST['num1'] - $_POST['num2'];
-    $pro  = $_POST['num1'] * $_POST['num2']; 
-    $quo  = $_POST['num1'] / $_POST['num2'];
-
-    switch ($_POST['operator']) {
-        case '+':
-            echo $_POST['num1'] . '+' . $_POST['num2'] . '=' . $sum;
-            break;
-        case '-':
-            echo $_POST['num1'] . '-' . $_POST['num2'] . '=' . $diff;
-            break;
-        case '×':
-            echo $_POST['num1'] . '×' . $_POST['num2'] . '=' . $pro;
-            break;
-        default:
-            echo $_POST['num1'] . '÷' . $_POST['num2'] . '=' . $quo;
-            break;
+    if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+        switch ($operator) {
+            case '+':
+                echo $num1 . '+' . $num2 . '=' . $sum;
+                break;
+            case '-':
+                echo $num1 . '-' . $num2 . '=' . $diff;
+                break;
+            case '×':
+                echo $num1 . '×' . $num2 . '=' . $pro;
+                break;
+            default:
+                echo $num1 . '÷' . $num2 . '=' . $quo;
+                break;
+        }
     }
     ?>
     <br><br>
